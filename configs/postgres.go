2e5627476bc8b5e763105940c54a73a192970e66
donghquinn <ehdgus1524@gmail.com>
2026-03-02 11:55:43 +0900

    added: postgres

---
+++ configs/postgres.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type PostgresConf struct {
+	// PostgreSQL
+	DbHost   string `envconfig:"POSTGRES_HOST" validate:"required"`
+	DbPort   string `envconfig:"POSTGRES_PORT" default:"5432"`
+	DbName   string `envconfig:"POSTGRES_NAME" validate:"required"`
+	DbUser   string `envconfig:"POSTGRES_USER" validate:"required"`
+	DbPasswd string `envconfig:"POSTGRES_PASSWD" validate:"required"`
+}
+
+var PostgresConfig PostgresConf
+
+func ReadDatabaseCfg() error {
+	if err := envconfig.Process("POSTGRES", &PostgresConfig); err != nil {
+		return fmt.Errorf("read postgres cfg err: %v", err)
+	}
+	return nil
+}
+
+func (g *PostgresConf) Validate() error {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(g); err != nil {
+		return fmt.Errorf("validate postgres cfg err: %v", err)
+	}
+	return nil
+}
