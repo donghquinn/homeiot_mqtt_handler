f6b1574177c6a86e40c3b2448db9162d52ef67df
donghquinn <ehdgus1524@gmail.com>
2026-03-02 12:16:08 +0900

    postgres client

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,59 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/donghquinn/gqbd"
+	_ "github.com/lib/pq"
+	"org.donghyuns.com/mqtt/listner/configs"
+)
+
+type PostgresService struct {
+	Client *sql.DB
+}
+
+func NewPostgresConnector() (*PostgresService, error) {
+	cfg := configs.PostgresCfg
+
+	sslMode := "disable"
+	maxConn := 20
+	maxIdleconn := 10
+	maxLifeTime := 30 * time.Minute
+	maxIdleLifeTime := 5 * time.Minute
+
+	port, err := strconv.Atoi(cfg.DbPort)
+	if err != nil {
+		port = 5432
+	}
+
+	conn, err := sql.Open("postgres", gqbd.BuildConnectionString(gqbd.PostgreSQL, gqbd.DBConfig{
+		Host:     cfg.DbHost,
+		Port:     port,
+		Password: cfg.DbPasswd,
+		User:     cfg.DbUser,
+		DBName:   cfg.DbName,
+		SSLMode:  sslMode,
+	}))
+	if err != nil {
+		return nil, fmt.Errorf("postgres connector init err: %v", err)
+	}
+
+	conn.SetMaxOpenConns(maxConn)
+	conn.SetMaxIdleConns(maxIdleconn)
+	conn.SetConnMaxLifetime(maxLifeTime)
+	conn.SetConnMaxIdleTime(maxIdleLifeTime)
+
+	return &PostgresService{
+		Client: conn,
+	}, nil
+}
+
+func (p *PostgresService) CheckConnection() error {
+	if err := p.Client.Ping(); err != nil {
+		return fmt.Errorf("check database connection err: %v", err)
+	}
+	return nil
+}
