dd59d513c7d1ffdb7391d4c7a3fa3df0eba36f30
donghquinn <ehdgus1524@gmail.com>
2026-03-02 11:30:10 +0900

    wip: mqtt configs

--- configs/mqtt.go
+++ configs/mqtt.go
@@ -1,1 +1,7 @@
 package configs
+
+type MqttConfig struct {
+	Topic    string `envconfig:"MQTT_TOPIC" validate:"required"`
+	Qos      int    `envconfig:"MQTT_QOS" default:"1"`
+	ClientId string `envconfig:"MQTT_CLIENT_ID" validate:"required"`
+}
