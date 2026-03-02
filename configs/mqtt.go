0b7577aff1b758c8a2e094fd7e1aee5d461f404a
donghquinn <ehdgus1524@gmail.com>
2026-03-02 11:39:21 +0900

    added: MQTT configs

--- configs/mqtt.go
+++ configs/mqtt.go
@@ -12,6 +12,15 @@ type MqttConfig struct {
 	Topic    string `envconfig:"MQTT_TOPIC" validate:"required"`
 	Qos      int    `envconfig:"MQTT_QOS" default:"1"`
 	ClientId string `envconfig:"MQTT_CLIENT_ID" validate:"required"`
+
+	OrderMatters bool `envconfig:"MQTT_ORDER_MATTERS" default:"false"` // 순서가 중요할 때
+	Timeout      int  `envconfig:"MQTT_TIMEOUT" default:"10"`
+	WriteTimeout int  `envconfig:"MQTT_WRITE_TIMEOUT" default:"1"`
+	KeepAlive    int  `envconfig:"MQTT_KEEP_ALIVE" default:"10"`
+	PingTimeout  int  `envconfig:"MQTT_PING_TIMEOUT" default:"1"`
+
+	ConnectRetry  bool `envconfig:"MQTT_CONNECT_RETRY" default:"true"`
+	AutoReconnect bool `envconfig:"MQTT_AUTO_RECONNECT" default:"true"`
 }
 
 var MqttCfg MqttConfig
