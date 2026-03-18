edfe607d5e146f522df8c0982d6d65e1c505e44c
donghquinn <ehdgus1524@gmail.com>
2026-03-18 22:33:14 +0900

    log path

--- configs/logger.go
+++ configs/logger.go
@@ -4,5 +4,5 @@ type LogConfig struct {
 	Level      string `env:"AS_LOG_LEVEL" envDefault:"debug"`
 	OutputType string `env:"AS_LOG_OUTPUT_TYPE" envDefault:"both"`
 	MaxSize    int    `env:"AS_LOG_MAX_SIZE" envDefault:"31457280"`
-	Path       string `env:"AS_LOG_PATH" envDefault:"/home/node/logs/saju.log"`
+	Path       string `env:"AS_LOG_PATH" envDefault:"/home/node/logs/homeiot.log"`
 }
